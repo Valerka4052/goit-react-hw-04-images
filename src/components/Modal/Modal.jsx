03184bba93d38acbae1c7d28ab7c0125682b9fb7
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-10 19:16:32 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,17 @@
 import PropTypes from 'prop-types';
 import { Overlay, ModalContainer } from './Modal.styled';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-    static propTypes = {
-        LargeImage: PropTypes.string.isRequired,
-        toggleModal: PropTypes.func.isRequired,
-    };
-    
-    modalEventLisetener = (e) => {
-        if (e.target.id === 'overlay') { return this.props.toggleModal() };
+export function Modal ({LargeImage, setShowModal}) {
+   
+   const modalEventLisetener = (e) => {
+        if (e.target.id === 'overlay') { return setShowModal(false) };
     };
 
-    render() {
-
-        const { props: { LargeImage }, modalEventLisetener } = this;
-
-        return createPortal(<Overlay onClick={modalEventLisetener} id='overlay'>
+    return createPortal(
+        <Overlay onClick={modalEventLisetener} id='overlay'>
             <ModalContainer>
                 <img src={LargeImage} alt="" />
             </ModalContainer>
@@ -27,7 +19,11 @@ export class Modal extends Component {
             ModalRoot
         );
     };
-};
+ Modal.propTypes = {
+        LargeImage: PropTypes.string.isRequired,
+        toggleModal: PropTypes.func.isRequired,
+    };
+    
 
 
 
