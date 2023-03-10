77bae6224f97c6219d8e16a3f8603f97ee71dbc6
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-10 18:59:14 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import { getApi, ItemsPerPage } from '../api'
 import { SearchBar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery'
@@ -9,16 +8,6 @@ import Notiflix from 'notiflix';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
-  // state = {
-  //   pictures: [],
-  //   searchQuerry: '',
-  //   page: 1,
-  //   largeImage: "",
-  //   showModal: false,
-  //   loading: false,
-  //   lastPage: false,
-  // };
-
   const [pictures, setPictures] = useState([]);
   const [searchQuerry, setSearchQuerry] = useState('');
   const [page, setPage] = useState(1);
@@ -30,135 +19,49 @@ export const App = () => {
   useEffect(() => {
     console.log('useeffect')
     if (page !== 1 ) {
-       console.log('load more')
-    setLoading(true);getApi(searchQuerry, page).then((array) => {
-        if (array.length < ItemsPerPage) {setLastPage(true);};setPictures([...pictures, ...array]);setLoading(false);});
+      console.log('load more');
+      setLoading(true);
+      getApi(searchQuerry, page)
+        .then((array) => {
+          if (array.length < ItemsPerPage) {
+            setLastPage(true);
+          };
+          setPictures([...pictures, ...array]);
+          setLoading(false);
+        });
     } else if (searchQuerry !== '') {
-      console.log('search')
-       if (searchQuerry === '') { return console.log('exit search')}
-    if (pictures.length > 0) {setPictures([])}; setLoading(true);getApi(searchQuerry, page)
+      console.log('search');
+      if (searchQuerry === '') {
+        return console.log('exit search')
+      }
+      if (pictures.length > 0) {
+        setPictures([])
+      };
+      setLoading(true);
+      getApi(searchQuerry, page)
       .then((array) => {
-        if (array.length < ItemsPerPage && array.length > 0) {setLastPage(true)};
-        if (array.length === ItemsPerPage) {setLastPage(false)};
-        if (array.length) { setPictures(array) }
-        else {Notiflix.Notify.failure('Please enter valid search querry'); setPictures([]);};setLoading(false);})
+        if (array.length < ItemsPerPage && array.length > 0) {
+          setLastPage(true);
+        };
+        if (array.length === ItemsPerPage) {
+          setLastPage(false)
+        };
+        if (array.length) {
+          setPictures(array)
+        }
+        else {
+          Notiflix.Notify.failure('Please enter valid search querry');
+          setPictures([]);
+        };
+        setLoading(false);
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }}, [searchQuerry,page]);
-
-  // useEffect(() => {
-  //   console.log('search')
-  //   if (searchQuerry === '') { return console.log('exit search')}
-  //   if (pictures.length > 0) {setPictures([])}; setLoading(true);getApi(searchQuerry, page)
-  //     .then((array) => {
-  //       if (array.length < ItemsPerPage && array.length > 0) {setLastPage(true)};
-  //       if (array.length === ItemsPerPage) {setLastPage(false)};
-  //       if (array.length) { setPictures(array) }
-  //       else {Notiflix.Notify.failure('Please enter valid search querry'); setPictures([]);};setLoading(false);})
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   }, [searchQuerry]);
-
-
+    }
+  }, [searchQuerry, page]);
   
-
-//   useEffect(() => {
-//   if (page !==1  && searchQuerry !== '') {
-//         // Дії для кнопки Load More
-//      setLoading(true);
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage) {
-//             setLastPage(true)
-//             // this.setState({ lastPage: true })
-//           };
-//            setPictures([...pictures, ...array]);
-//           // this.setState(prevState => {
-//           //   return { pictures: [...prevState.pictures, ...array] }
-//           // });
-//           setLoading(false);
-//         });
-//     } else if (searchQuerry !== '') {
-//       // Дії для searchBar
-//     if (pictures.length > 0) {
-//         setPictures([]);
-//         // this.setState({ pictures: [] })
-//       };
-//       setLoading(true);
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage && array.length > 0) {
-// setLastPage(true)
-//             // this.setState({ lastPage: true })
-//           };
-//           if (array.length === ItemsPerPage) {
-//             setLastPage(true)
-//             // this.setState({ lastPage: false })
-//           };
-//           if (array.length) {
-//              setPictures([array]);
-//             // this.setState({ pictures: array })
-//           } else {
-//             Notiflix.Notify.failure('Please enter valid search querry');
-//             setPictures([array]);
-//             // this.setState({ pictures: [] })
-//           };
-//           setLoading(false);
-//         })
-//     };
-// },[page, pictures, searchQuerry])
-
-
-//  componentDidUpdate(_, prevState) {
-//   const { state: { page, searchQuerry }, toggleLoading } = this;
-//     if (page > prevState.page && searchQuerry === prevState.searchQuerry) {
-//         // Дії для кнопки Load More
-//       toggleLoading();
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage) {
-//             this.setState({ lastPage: true })
-//           };
-//           this.setState(prevState => {
-//             return { pictures: [...prevState.pictures, ...array] }
-//           });
-//           toggleLoading();
-//         });
-//     } else if (searchQuerry !== prevState.searchQuerry) {
-//       // Дії для searchBar
-//       if (prevState.pictures.length > 0){
-//         this.setState({ pictures: [] })
-//       };
-//       toggleLoading();
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage && array.length > 0) {
-//             this.setState({ lastPage: true })
-//           };
-//           if (array.length === ItemsPerPage) {
-//             this.setState({ lastPage: false })
-//           };
-//           if (array.length) {
-//             this.setState({ pictures: array })
-//           } else {
-//             Notiflix.Notify.failure('Please enter valid search querry');
-//             this.setState({ pictures: [] })
-//           };
-//           toggleLoading();
-//         })
-//     };
-//   };
- 
-  // function toggleModal() {
-    // setShowModal(!showModal);
-    // this.setState(({ showModal }) => ({ showModal: !showModal }));
-  // }
-//  const toggleLoading = () => {
-//   setLoading(!loading)
-//     // this.setState(({ loading }) => ({ loading: !loading }));
-//   };
 
   const getLargeImage = (e) => {
    setLargeImage(e.target.id)
-    // this.setState({ largeImage: e.target.id });
     if(e.target.nodeName==='IMG'){
       setShowModal(true);
     };
@@ -167,28 +70,17 @@ export const App = () => {
  const searchQuerryToState = (value) => {
    if (value === '') {
       setPictures([])
-      // this.setState({ pictures: [] });
       Notiflix.Notify.warning('Please enter the search querry');
       return;
    };
    setSearchQuerry(value);
    setPage(1)
-    // this.setState({
-    //   searchQuerry: value,
-    //   page: 1,
-    // });
   };
 
   const loadMore = () => {
     setPage(page + 1);
-    // this.setState(({ page }) => { return { page: page + 1 } });
   };
   
-  
-
-
-    // const {toggleModal, searchQuerryToState, loadMore, getLargeImage, state: {lastPage, pictures, largeImage, showModal, loading } } = this;
-
     return (
       <div
         style={{
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,14 @@
 import PropTypes from 'prop-types';
 import { Overlay, ModalContainer } from './Modal.styled';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-    static propTypes = {
-        LargeImage: PropTypes.string.isRequired,
-        toggleModal: PropTypes.func.isRequired,
+export function Modal ({setShowModal,LargeImage}) {
+        
+ const  modalEventLisetener = function  (e)  {
+        if (e.target.id === 'overlay') { return setShowModal(false) };
     };
-    
-    modalEventLisetener = (e) => {
-        if (e.target.id === 'overlay') { return this.props.setShowModal(false) };
-    };
-
-    render() {
-
-        const { props: { LargeImage }, modalEventLisetener } = this;
 
         return createPortal(<Overlay onClick={modalEventLisetener} id='overlay'>
             <ModalContainer>
@@ -27,7 +18,11 @@ export class Modal extends Component {
             ModalRoot
         );
     };
-};
+
+Modal.propTypes = {
+        LargeImage: PropTypes.string.isRequired,
+        toggleModal: PropTypes.func.isRequired,
+    };
 
 
 
