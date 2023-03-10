2e666bf46f151b49021d8daaac1bc02f72e764bb
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-10 17:57:31 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import { Component } from 'react';
+import { Component } from 'react';
 import { getApi, ItemsPerPage } from '../api'
 import { SearchBar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery'
@@ -6,97 +6,92 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import Notiflix from 'notiflix';
-import { useState, useEffect } from 'react';
 
-export const App = () =>{
-  const [pictures, setPictures] = useState([]);
-  const [searchQuerry, setSearchQuerry] = useState('');
-  const [page, setPage] = useState(1);
-  const [largeImage, setLargeImage] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [lastPage, setLastPage,] = useState(false);
+export class App extends Component{
+  state = {
+    pictures: [],
+    searchQuerry: '',
+    page: 1,
+    largeImage: "",
+    showModal: false,
+    loading: false,
+    lastPage: false,
+  };
 
-//  componentDidUpdate(_, prevState) {
-//   const { state: { page, searchQuerry }, toggleLoading } = this;
-//     if (page > prevState.page && searchQuerry === prevState.searchQuerry) {
-//         // Дії для кнопки Load More
-//       toggleLoading();
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage) {
-//             this.setState({ lastPage: true })
-//           };
-//           this.setState(prevState => {
-//             return { pictures: [...prevState.pictures, ...array] }
-//           });
-//           toggleLoading();
-//         });
-//     } else if (searchQuerry !== prevState.searchQuerry) {
-//       // Дії для searchBar
-//       if (prevState.pictures.length > 0){
-//         this.setState({ pictures: [] })
-//       };
-//       toggleLoading();
-//       getApi(searchQuerry, page)
-//         .then((array) => {
-//           if (array.length < ItemsPerPage && array.length > 0) {
-//             this.setState({ lastPage: true })
-//           };
-//           if (array.length === ItemsPerPage) {
-//             this.setState({ lastPage: false })
-//           };
-//           if (array.length) {
-//             this.setState({ pictures: array })
-//           } else {
-//             Notiflix.Notify.failure('Please enter valid search querry');
-//             this.setState({ pictures: [] })
-//           };
-//           toggleLoading();
-//         })
-//     };
-//   };
+ componentDidUpdate(_, prevState) {
+  const { state: { page, searchQuerry }, toggleLoading } = this;
+    if (page > prevState.page && searchQuerry === prevState.searchQuerry) {
+        // Дії для кнопки Load More
+      toggleLoading();
+      getApi(searchQuerry, page)
+        .then((array) => {
+          if (array.length < ItemsPerPage) {
+            this.setState({ lastPage: true })
+          };
+          this.setState(prevState => {
+            return { pictures: [...prevState.pictures, ...array] }
+          });
+          toggleLoading();
+        });
+    } else if (searchQuerry !== prevState.searchQuerry) {
+      // Дії для searchBar
+      if (prevState.pictures.length > 0){
+        this.setState({ pictures: [] })
+      };
+      toggleLoading();
+      getApi(searchQuerry, page)
+        .then((array) => {
+          if (array.length < ItemsPerPage && array.length > 0) {
+            this.setState({ lastPage: true })
+          };
+          if (array.length === ItemsPerPage) {
+            this.setState({ lastPage: false })
+          };
+          if (array.length) {
+            this.setState({ pictures: array })
+          } else {
+            Notiflix.Notify.failure('Please enter valid search querry');
+            this.setState({ pictures: [] })
+          };
+          toggleLoading();
+        })
+    };
+  };
  
-// const  toggleModal = () => {
-    // setShowModal(!showModal)
-    // this.setState(({ showModal }) => ({ showModal: !showModal }));
-  // };
-  // toggleLoading = () => {
-  //   this.setState(({ loading }) => ({ loading: !loading }));
-  // };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+  toggleLoading = () => {
+    this.setState(({ loading }) => ({ loading: !loading }));
+  };
 
-const  getLargeImage = (e) => {
-    setLargeImage(e.target.id)
-    // this.setState({ largeImage: e.target.id });
+  getLargeImage = (e) => {
+    this.setState({ largeImage: e.target.id });
     if(e.target.nodeName==='IMG'){
-    setShowModal(true)
+      this.toggleModal();
     };
   };
 
-const  searchQuerryToState = (value) => {
+  searchQuerryToState = (value) => {
     if (value === '') {
-      setPictures([]);
-      // this.setState({ pictures: [] });
+      this.setState({ pictures: [] });
       Notiflix.Notify.warning('Please enter the search querry');
       return;
     };
-    // setSearchQuerry(value);
-    setPage(1);
-    // this.setState({
-    //   searchQuerry: value,
-    //   page: 1,
-    // });
+    this.setState({
+      searchQuerry: value,
+      page: 1,
+    });
   };
 
-const  loadMore = () => {
-    setPage(page + 1)
-    // this.setState(({ page }) => { return { page: page + 1 } });
+  loadMore = () => {
+    this.setState(({ page }) => { return { page: page + 1 } });
   };
   
   
-  // render() {
+  render() {
 
-    // const {toggleModal, searchQuerryToState, loadMore, getLargeImage, state: {lastPage, pictures, largeImage, showModal, loading } } = this;
+    const {toggleModal, searchQuerryToState, loadMore, getLargeImage, state: {lastPage, pictures, largeImage, showModal, loading } } = this;
 
     return (
       <div
@@ -116,11 +111,11 @@ const  loadMore = () => {
         {!loading && pictures.length > 0 && !lastPage && <Button loadMore={loadMore} />}
         {showModal && <Modal
           LargeImage={largeImage}
-          setShowModal={setShowModal}
+          toggleModal={toggleModal}
         />}
       </div>
     );
   };
-// };
+};
 
 
