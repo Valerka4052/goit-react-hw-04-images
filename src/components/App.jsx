06f43b1003a4719d36096d8d33bf3b7e3c34d35d
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-10 15:19:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { getApi, ItemsPerPage } from '../api'
 import { SearchBar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery'
@@ -6,92 +6,107 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import Notiflix from 'notiflix';
+import { useState, useEffect } from 'react';
 
-export class App extends Component{
-  state = {
-    pictures: [],
-    searchQuerry: '',
-    page: 1,
-    largeImage: "",
-    showModal: false,
-    loading: false,
-    lastPage: false,
-  };
+export function App (){
+  // state = {
+  //   pictures: [],
+  //   searchQuerry: '',
+  //   page: 1,
+  //   largeImage: "",
+  //   showModal: false,
+  //   loading: false,
+  //   lastPage: false,
+  // };
 
- componentDidUpdate(_, prevState) {
-  const { state: { page, searchQuerry }, toggleLoading } = this;
-    if (page > prevState.page && searchQuerry === prevState.searchQuerry) {
-        // Дії для кнопки Load More
-      toggleLoading();
-      getApi(searchQuerry, page)
-        .then((array) => {
-          if (array.length < ItemsPerPage) {
-            this.setState({ lastPage: true })
-          };
-          this.setState(prevState => {
-            return { pictures: [...prevState.pictures, ...array] }
-          });
-          toggleLoading();
-        });
-    } else if (searchQuerry !== prevState.searchQuerry) {
-      // Дії для searchBar
-      if (prevState.pictures.length > 0){
-        this.setState({ pictures: [] })
-      };
-      toggleLoading();
-      getApi(searchQuerry, page)
-        .then((array) => {
-          if (array.length < ItemsPerPage && array.length > 0) {
-            this.setState({ lastPage: true })
-          };
-          if (array.length === ItemsPerPage) {
-            this.setState({ lastPage: false })
-          };
-          if (array.length) {
-            this.setState({ pictures: array })
-          } else {
-            Notiflix.Notify.failure('Please enter valid search querry');
-            this.setState({ pictures: [] })
-          };
-          toggleLoading();
-        })
-    };
-  };
+  const [pictures, setPictures] = useState([]);
+  const [searchQuerry, setSearchQuerry] = useState('');
+  const [page, setPage] = useState(1);
+  const [largeImage, setLargeImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [lastPage, setLastPage,] = useState(false);
+
+//  componentDidUpdate(_, prevState) {
+//   const { state: { page, searchQuerry }, toggleLoading } = this;
+//     if (page > prevState.page && searchQuerry === prevState.searchQuerry) {
+//         // Дії для кнопки Load More
+//       toggleLoading();
+//       getApi(searchQuerry, page)
+//         .then((array) => {
+//           if (array.length < ItemsPerPage) {
+//             this.setState({ lastPage: true })
+//           };
+//           this.setState(prevState => {
+//             return { pictures: [...prevState.pictures, ...array] }
+//           });
+//           toggleLoading();
+//         });
+//     } else if (searchQuerry !== prevState.searchQuerry) {
+//       // Дії для searchBar
+//       if (prevState.pictures.length > 0){
+//         this.setState({ pictures: [] })
+//       };
+//       toggleLoading();
+//       getApi(searchQuerry, page)
+//         .then((array) => {
+//           if (array.length < ItemsPerPage && array.length > 0) {
+//             this.setState({ lastPage: true })
+//           };
+//           if (array.length === ItemsPerPage) {
+//             this.setState({ lastPage: false })
+//           };
+//           if (array.length) {
+//             this.setState({ pictures: array })
+//           } else {
+//             Notiflix.Notify.failure('Please enter valid search querry');
+//             this.setState({ pictures: [] })
+//           };
+//           toggleLoading();
+//         })
+//     };
+//   };
  
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+const  toggleModal = () => {
+    setShowModal(!showModal)
+    // this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
-  toggleLoading = () => {
-    this.setState(({ loading }) => ({ loading: !loading }));
-  };
+  // toggleLoading = () => {
+  //   this.setState(({ loading }) => ({ loading: !loading }));
+  // };
 
-  getLargeImage = (e) => {
-    this.setState({ largeImage: e.target.id });
+const  getLargeImage = (e) => {
+    setLargeImage(e.target.id)
+    // this.setState({ largeImage: e.target.id });
     if(e.target.nodeName==='IMG'){
       this.toggleModal();
     };
   };
 
-  searchQuerryToState = (value) => {
+const  searchQuerryToState = (value) => {
     if (value === '') {
-      this.setState({ pictures: [] });
+      setPictures([])
+      // this.setState({ pictures: [] });
       Notiflix.Notify.warning('Please enter the search querry');
       return;
     };
-    this.setState({
-      searchQuerry: value,
-      page: 1,
-    });
+    setSearchQuerry(value);
+    setPage(1)
+    // this.setState({
+    //   searchQuerry: value,
+    //   page: 1,
+    // });
   };
 
-  loadMore = () => {
-    this.setState(({ page }) => { return { page: page + 1 } });
+const  loadMore = () => {
+    setPage(page + 1)
+    // this.setState(({ page }) => { return { page: page + 1 } });
   };
   
   
-  render() {
+  // render() {
 
-    const {toggleModal, searchQuerryToState, loadMore, getLargeImage, state: {lastPage, pictures, largeImage, showModal, loading } } = this;
+    // const {toggleModal, searchQuerryToState, loadMore, getLargeImage, state: {lastPage, pictures, largeImage, showModal, loading } } = this;
 
     return (
       <div
@@ -116,6 +131,6 @@ export class App extends Component{
       </div>
     );
   };
-};
+// };
 
 
